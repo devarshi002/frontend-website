c053dcd4bc8c3551e5222b07b9bbcd7a85d58cc6
devarshi002 <devarshitambulkar2@gmail.com>
2024-03-29 23:14:24 +0530

    page2

--- script.js
+++ script.js
@@ -42,4 +42,37 @@ nav.addEventListener("mouseleave", function () {
 })
 }
 
-navAnimation()
+// navAnimation()
+
+
+
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem")
+
+rightElems.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        })
+
+    })
+
+    elem.addEventListener("mousemove", function(dets){
+
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x-50,
+            y:dets.y - elem.getBoundingClientRect().y-150
+        })
+    })
+})
+}
+
+page2Animation()
