f0c9bc1f1af6bf5b0cc65d2a2482642d5e892125
devarshi002 <devarshitambulkar2@gmail.com>
2024-03-30 20:11:49 +0530

    page4

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ nav.addEventListener("mouseleave", function () {
 })
 }
 
-// navAnimation()
 
 
 
@@ -75,4 +74,52 @@ rightElems.forEach(function(elem){
 })
 }
 
+
+
+// page 3 video logic
+
+function page3VideoAnimation(){
+    var page3Center = document.querySelector(".page3-center")
+var video = document.querySelector("#page3 video")
+
+page3Center.addEventListener("click", function(){
+    video.play()
+    gsap.to(video,{
+        transform:"scaleX(1) scaleY(1)",
+        opacity:1,
+        borderRadius:0
+    })
+})
+
+video.addEventListener("click", function(){
+    video.pause()
+    gsap.to(video,{
+        transform:"scaleX(0.7) scaleY(0)",
+        opacity:0,
+        borderRadius:"30px"
+    })
+})
+}
+
+
+navAnimation()
+
 page2Animation()
+
+page3VideoAnimation()
+
+
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach (function(elem){
+    elem.addEventListener("mouseenter", function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()    
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()    
+    })
+})
